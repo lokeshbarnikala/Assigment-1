b534e4e62f6d13e06d44727c809488c693c588e9
lokeshbarnikala <170216060+lokeshbarnikala@users.noreply.github.com>
2024-05-23 11:49:21 +0530

    Add files via upload

---
+++ basic.c
@@ -0,0 +1,8 @@
+#include <stdio.h> // preprocessor directive
+int main() // Definition of main()begins 
+{
+printf("my self lokesh\nIam coming from krishna devi peta\nAddress:7-106/1,High school road\n");
+printf("what we learning\n");
+scanf("&d");
+return 0;
+}
