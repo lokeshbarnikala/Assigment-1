207b0adec048fdf2ea056cb42001501d7fcb0aec
lokeshbarnikala <170216060+lokeshbarnikala@users.noreply.github.com>
2024-05-23 11:52:55 +0530

    basic.c

--- basic.c
+++ basic.c
@@ -1,8 +1,7 @@
 #include <stdio.h> // preprocessor directive
 int main() // Definition of main()begins 
 {
-printf("my self lokesh\nIam coming from krishna devi peta\nAddress:7-106/1,High school road\n");
-printf("what we learning\n");
-scanf("&d");
-return 0;
+  printf("This is my first c program");
+  printf("My self lokesh\nIam coming from krishna devi peta\nAddress:7-106/1,High school road\n");
+  return 0;
 }
