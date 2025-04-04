531d176c979aabd65eb608273366433695934ed7
lokeshbarnikala <lokeshbarnikala34@gmail.com>
2025-04-04 21:36:26 +0530

    Delete basic.c

--- basic.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h> // preprocessor directive
-int main() // Definition of main()begins 
-{
-  printf("This is my first c program\n");
-  printf("My self lokesh\nIam coming from krishna devi peta\nAddress:7-106/1,High school road\n");
-  return 0;
-}
